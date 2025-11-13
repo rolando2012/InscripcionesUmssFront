f2d72a3839e755812118cc05a053058d6b56b981
rolando2012 <rolamdomr@gmail.com>
2025-11-13 00:31:32 -0400

    Agregar página de inscripciones y configurar fuentes personalizadas

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { TituloFont } from "../../config/fonts";
 
 export default function Home() {
   return (
@@ -58,6 +59,9 @@ export default function Home() {
           >
             Documentation
           </a>
+          <p className={`${TituloFont.className} font-bold`}>UMSS</p>
+          <p className={`${TituloFont.className} font-light`}>UMSS</p>
+          <p className={`${TituloFont.className} font-thin`}>UMSS</p>
         </div>
       </main>
     </div>
---
+++ src/config/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter} from 'next/font/google';
+
+export const TituloFont = Inter({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '600'],
+  variable: '--font-titulo',
+});
